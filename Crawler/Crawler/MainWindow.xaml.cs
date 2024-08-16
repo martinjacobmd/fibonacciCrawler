8c343672759cd6bb88a8a3d94ffcd20e27cee629
Martin Jacob <64908101+martinjacobmd@users.noreply.github.com>
2024-08-16 15:27:58 +0200

    initial

---
+++ Crawler/Crawler/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Crawler
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string url = "https://www.spreetropol.de/index.php/impressum/";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// Clickhandler to start the process
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void crawlButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultList.Items.Clear();
+            crawlButton.IsEnabled = false;
+            crawlButton.Content = "processing";
+            this.StartCrawlingAsync();
+        }
+
+        /// <summary>
+        /// Method that contains the "BI" of this programm
+        /// </summary>
+        /// <returns></returns>
+        private async Task StartCrawlingAsync()
+        {
+            string websiteContent = await Utils.GetWebsite(url);
+            contentFrame.Content = websiteContent;
+            resultList.Items.Add($"crawled website: {url} ");
+
+            string number = Utils.ExtractPhoneNumber(websiteContent);
+            resultList.Items.Add($"found phone number: {number} ");
+
+            int secondQuartet = Int16.Parse(number.Substring(4, 4));
+            resultList.Items.Add($"found second quartet: {secondQuartet} ");
+
+            int numberFibonacci = Utils.CalculateFibonacci(secondQuartet);
+            resultList.Items.Add($"found fibonacci number: {numberFibonacci} ");
+            this.resultList.Visibility = Visibility.Visible;
+
+            crawlButton.IsEnabled = true;
+            crawlButton.Content = "Start Crawling";
+        }
+    }
+}
---
+++ Crawler/Crawler/Utils.cs
@@ -0,0 +1,54 @@
+﻿using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Crawler
+{
+    static class Utils
+    {
+        /// <summary>
+        /// Method that triggers a GET call to the desired website and loads the content
+        /// </summary>
+        /// <param name="url">the url of the website</param>
+        /// <returns></returns>
+        public static async Task<string> GetWebsite(string url)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(url);
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        /// <summary>
+        /// Method to extract the phone number from the content; trims out unnecessary chars
+        /// </summary>
+        /// <param name="content">The content</param>
+        /// <returns>A string only containing the 4 digits from the number</returns>
+        public static string ExtractPhoneNumber(string content)
+        {
+            string regEx = @"(?:\bTelefon\b)+([0-9\s:]+)";
+            Regex r = new Regex(regEx, RegexOptions.IgnoreCase);
+            Match m = r.Match(content);
+
+            return m.Groups[1].Value.Replace(":", "").Replace(" ", "");
+        }
+
+        /// <summary>
+        /// Method calculated the x-th fibonacci number
+        /// </summary>
+        /// <param name="number">The x-th number</param>
+        /// <returns>The fibonacci number</returns>
+        public static int CalculateFibonacci(int number)
+        {
+            int a = 0, b = 1, temp;
+
+            for (int i = 0; i < number; i++)
+            {
+                temp = a;
+                a = b;
+                b = temp + b;
+            }
+
+            return a;
+        }
+    }
+}
